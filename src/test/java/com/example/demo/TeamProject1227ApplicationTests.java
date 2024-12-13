94d7190b3055b28fe23d3711df9fba28245e13d4
3030455 <3030455@30304-55>
2024-12-13 11:23:19 +0900

    新規追加

---
+++ src/test/java/com/example/demo/TeamProject1227ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProject1227ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
