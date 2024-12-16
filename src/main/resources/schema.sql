8189f11716c85ad70c85f367b8bca6eab22ee063
3030739 <3030739@30307-39>
2024-12-16 10:42:50 +0900

    schema.sqlの修正

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,12 @@
---テーブルが存在したら削除する
-DROP TABLE IF EXISTS present_list
+-- テーブルが存在したら削除する
+DROP TABLE IF EXISTS present_list;
 
---テーブルの作成
-CREATE TABLE present_list(
-	--ID:主キー（自動生成）
-	id serial PRIMARY KEY,
-	--プレゼント番号:NULL不許可
-	present VARCHAR(1) NOT NULL,
-	--住所:NULL不許可
-	adress VARCHAR(250) NOT NULL
+-- テーブルの作成
+CREATE TABLE present_list (
+    -- ID: 主キー（自動生成）
+    id SERIAL PRIMARY KEY,
+    -- プレゼント番号: NULL不許可
+    present VARCHAR(1) NOT NULL,
+    -- 住所: NULL不許可
+    address VARCHAR(250) NOT NULL
+);
