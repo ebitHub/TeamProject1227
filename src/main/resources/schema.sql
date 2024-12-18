ccfd8c9f3e2a170f7be947c9662f12530a0fe31d
3030739 <3030739@30307-39>
2024-12-18 10:21:22 +0900

    Schema.sqlの型をIntegerに変更

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE present_list (
     -- ID: 主キー（自動生成）
     id SERIAL PRIMARY KEY,
     -- プレゼント番号: NULL不許可
-    present VARCHAR(1) NOT NULL,
+    present INTEGER NOT NULL,
     -- 住所: NULL不許可
     address VARCHAR(250) NOT NULL
 );
