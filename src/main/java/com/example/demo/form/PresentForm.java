94be7143e58d95f6276186ec96e671f1f1259ecc
ebitHub <chiho.t.9646@gmail.com>
2025-01-07 10:35:23 +0900

    コメント削除 PresentForm.java

--- src/main/java/com/example/demo/form/PresentForm.java
+++ src/main/java/com/example/demo/form/PresentForm.java
@@ -12,8 +12,8 @@ public class PresentForm {
 	/*入力データを一時的に保持するForm*/	
 	//プレゼント（NO1～6）
 	@NotNull(message = "数値が未入力です。")
-//	@Range(min = 1, max = 6, message = "{min}～{max}の数値を入力して下さい。")
 	private Integer present;
+	
 	//住所（アドレス）
 	@NotBlank(message = "住所は必須入力です。")
 	@Length(min = 1, max = 250, message = "住所は1文字以上、250文字以内で入力してください。")
